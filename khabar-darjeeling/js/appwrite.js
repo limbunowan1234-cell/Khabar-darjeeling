391654f4cfbb64f8d163a497dd981b5241d4bedf
limbunowan1234-cell <limbunowan1234@gmail.com>
2026-06-02 21:41:01 +0530

    Update appwrite.js

--- khabar-darjeeling/js/appwrite.js
+++ khabar-darjeeling/js/appwrite.js
@@ -3,19 +3,18 @@ const { Client, Databases, Storage, Account, ID } = window.Appwrite;
 
 const client = new Client()
     .setEndpoint('https://nyc.cloud.appwrite.io/v1')
-    .setProject('khabardarjeeling'); // Using your actual Project ID
+    .setProject('68b43d61001a7c0b6c68'); // ← Use the hex ID, not the name
 
 const databases = new Databases(client);
 const storage = new Storage(client);
 const account = new Account(client);
 
-// Make them global for admin.js
+// Make global
 window.account = account;
 window.database = databases;
 window.storage = storage;
 window.ID = ID;
 
-// Your Appwrite resource IDs
-const APPWRITE_DATABASE_ID = 'Khabar_db';
+const APPWRITE_DATABASE_ID = '68b44bf2000e26f021d3';
 const APPWRITE_COLLECTION_ID = 'articles';
 const APPWRITE_BUCKET_ID = 'article_images';
