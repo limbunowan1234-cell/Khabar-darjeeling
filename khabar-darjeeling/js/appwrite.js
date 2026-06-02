38a10e5152804f2e52f56905971b7134fd5d5e2a
limbunowan1234-cell <limbunowan1234@gmail.com>
2026-06-02 23:34:31 +0530

    Update appwrite.js

--- khabar-darjeeling/js/appwrite.js
+++ khabar-darjeeling/js/appwrite.js
@@ -1,4 +1,5 @@
-const { Client, Databases, Storage, Account, ID } = window.Appwrite;
+// js/appwrite.js
+const { Client, Databases, Storage, Account, ID, Query } = window.Appwrite;
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
@@ -8,14 +9,18 @@ const databases = new Databases(client);
 const storage = new Storage(client);
 const account = new Account(client);
 
-const APPWRITE_DB_ID = 'Khabar_db';
+// IMPORTANT: Check Appwrite Console → Database → Copy exact Database ID
+// Most likely 'khabar_db' lowercase, not 'Khabar_db'
+const APPWRITE_DB_ID = 'khabar_db'; // ← CHANGE THIS to match your actual DB ID
 const APPWRITE_COLLECTION_ID = 'articles';
 const APPWRITE_BUCKET_ID = 'article-images';
 
+// Make available globally
 window.account = account;
 window.database = databases;
 window.storage = storage;
 window.ID = ID;
+window.Query = Query;
 window.APPWRITE_DB_ID = APPWRITE_DB_ID;
 window.APPWRITE_COLLECTION_ID = APPWRITE_COLLECTION_ID;
 window.APPWRITE_BUCKET_ID = APPWRITE_BUCKET_ID;
