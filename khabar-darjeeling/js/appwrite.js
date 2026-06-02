25b1e6c6d068f91fdc7e7ba39e322a46d9252123
limbunowan1234-cell <limbunowan1234@gmail.com>
2026-06-02 23:22:27 +0530

    Update appwrite.js

--- khabar-darjeeling/js/appwrite.js
+++ khabar-darjeeling/js/appwrite.js
@@ -1,19 +1,21 @@
-// js/appwrite.js
 const { Client, Databases, Storage, Account, ID } = window.Appwrite;
 
 const client = new Client()
-    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
-    .setProject('khabardarjeeling'); // ← Your actual Project ID
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('khabardarjeeling');
 
 const databases = new Databases(client);
 const storage = new Storage(client);
 const account = new Account(client);
 
+const APPWRITE_DB_ID = 'Khabar_db';
+const APPWRITE_COLLECTION_ID = 'articles';
+const APPWRITE_BUCKET_ID = 'article-images';
+
 window.account = account;
 window.database = databases;
 window.storage = storage;
 window.ID = ID;
-
-const APPWRITE_DATABASE_ID = 'Khabar_db';
-const APPWRITE_COLLECTION_ID = 'articles';
-const APPWRITE_BUCKET_ID = 'article_images';
+window.APPWRITE_DB_ID = APPWRITE_DB_ID;
+window.APPWRITE_COLLECTION_ID = APPWRITE_COLLECTION_ID;
+window.APPWRITE_BUCKET_ID = APPWRITE_BUCKET_ID;
