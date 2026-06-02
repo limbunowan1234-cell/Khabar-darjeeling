0e69f33dd203a678b7336c78d50e554eb996d7b3
limbunowan1234-cell <limbunowan1234@gmail.com>
2026-06-02 14:35:53 +0530

    Update appwrite.js

--- khabar-darjeeling/js/appwrite.js
+++ khabar-darjeeling/js/appwrite.js
@@ -1,13 +1,11 @@
 const appwriteClient = new window.Appwrite.Client()
-    .setEndpoint(APPWRITE_ENDPOINT)
-    .setProject(APPWRITE_PROJECT_ID);
+   .setEndpoint(APPWRITE_ENDPOINT)
+   .setProject(APPWRITE_PROJECT_ID);
 
 const appwriteAccount = new window.Appwrite.Account(appwriteClient);
 const appwriteDatabase = new window.Appwrite.Databases(appwriteClient);
 const appwriteStorage = new window.Appwrite.Storage(appwriteClient);
 
-// Make them global so admin.js can use them
 window.account = appwriteAccount;
 window.database = appwriteDatabase;
 window.storage = appwriteStorage;
-window.Appwrite = window.Appwrite; // for Query
