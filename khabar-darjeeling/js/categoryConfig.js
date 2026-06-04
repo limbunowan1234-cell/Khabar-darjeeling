d9e6f622e5d22cfe17c79d70e827afea46a41671
limbunowan1234-cell <limbunowan1234@gmail.com>
2026-06-04 16:16:43 +0530

    Create categoryConfig.js

---
+++ khabar-darjeeling/js/categoryConfig.js
@@ -0,0 +1,13 @@
+// categoryConfig.js
+export const categoryThemes = {
+  "darjeeling": { color: "#2d5a27", banner: "/assets/banners/darjeeling.jpg", icon: "mountain-icon.png" },
+  "kalimpong": { color: "#3a7d44", banner: "/assets/banners/kalimpong.jpg", icon: "mountain-icon.png" },
+  "kurseong": { color: "#458b53", banner: "/assets/banners/kurseong.jpg", icon: "mountain-icon.png" },
+  "mirik": { color: "#2d5a27", banner: "/assets/banners/mirik.jpg", icon: "mountain-icon.png" },
+  "siliguri": { color: "#5d6d7e", banner: "/assets/banners/siliguri.jpg", icon: "city-icon.png" },
+  "west-bengal": { color: "#5d6d7e", banner: "/assets/banners/wb.jpg", icon: "map-icon.png" },
+  "national": { color: "#1a5276", banner: "/assets/banners/national.jpg", icon: "news-icon.png" },
+  "entertainment": { color: "#884ea0", banner: "/assets/banners/entertainment.jpg", icon: "star-icon.png" },
+  "sports": { color: "#e63946", banner: "/assets/banners/sports.jpg", icon: "trophy-icon.png" },
+  "default": { color: "#666666", banner: "/assets/banners/default.jpg", icon: "default-icon.png" }
+};
